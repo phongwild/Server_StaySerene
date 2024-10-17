335034517aaf739f02ccc1d11d6230c0c824ffc7
dungnt19704 <nguyendung190704@gmail.com>
2024-10-17 08:33:22 +0700

    layout chamsoc

---
+++ model/chamSoc_model.js
@@ -0,0 +1,19 @@
+const { default: mongoose } = require('mongoose');
+var db = require('./db');
+const chamSocSchema = new db.mongoose.Schema(
+    {
+        IdNhanVien: { type: mongoose.Schema.ObjectId, required: true, ref:'NhanVien' },
+        Uid: { type: mongoose.Schema.ObjectId, required: true, ref:'KhachHang' },
+        thoiGianGui: { type: String, required: false },
+        noiDungGui: { type: String, required: false },
+        vaiTro: { type: String, required: false },
+        trangThaiKh: { type: Number, required: false },
+        trangThaiNv: { type: Number, required: false },
+    },
+    {
+        collection: 'ChamSoc'
+    }
+);
+// tao model
+let ChamSocModel = db.mongoose.model('ChamSocModel', chamSocSchema);
+module.exports = { ChamSocModel }
