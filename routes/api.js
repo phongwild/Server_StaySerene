b08d43d3d33b1c1915f67bb2e188682066516288
Trần Anh Phong <118454061+phongwild@users.noreply.github.com>
2024-12-04 14:14:24 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const apiCtrl = require('../controllers/api.ctrl');
-const upload = require('../controllers/fileUpload');
 
 // Home route
 router.get('/', (req, res) => {
