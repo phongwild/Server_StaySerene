bdcb7f57d68dacaa224989f7513228d8019a6293
dungnt19704 <nguyendung190704@gmail.com>
2024-12-06 00:36:39 +0700

    update

--- views/html/js/dangnhapnv.js
+++ views/html/js/dangnhapnv.js
@@ -5,11 +5,7 @@ async function login() {
     const password = document.getElementById('password').value;
 
     try {
-<<<<<<< HEAD
         const response = await fetch('http://192.168.1.7:3000/api/nhanvien');
-=======
-        const response = await fetch('http://10.62.4.33:3000/api/nhanvien');
->>>>>>> 0ef55029b447dc3f664d7b39d9dbe89048bc4380
         const employees = await response.json();
         const employee = employees.find(emp => emp.email === email && emp.password === password);
 
